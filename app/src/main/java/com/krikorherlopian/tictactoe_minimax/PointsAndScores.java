a0df4d7ed8d01818fa0ad5a209e3932417217a19
Krikor Herlopian <kherl1@Unh.newhaven.edu>
2020-11-17 13:43:07 -0500

    commit

---
+++ app/src/main/java/com/krikorherlopian/tictactoe_minimax/PointsAndScores.java
@@ -0,0 +1,12 @@
+package com.krikorherlopian.tictactoe_minimax;
+
+public class PointsAndScores {
+
+    int score;
+    Point point;
+
+    PointsAndScores(int score, Point point) {
+        this.score = score;
+        this.point = point;
+    }
+}
